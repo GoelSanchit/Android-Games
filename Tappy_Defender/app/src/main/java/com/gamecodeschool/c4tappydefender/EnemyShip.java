0fb3d6bb6af81bf11d96cdf24d308a2fb2fd5307
Sanchit Goel <sanchit.mgoel007@gmail.com>
2016-03-08 21:40:59 +0530

    Compress

--- Tappy_Defender/app/src/main/java/com/gamecodeschool/c4tappydefender/EnemyShip.java
+++ Tappy_Defender/app/src/main/java/com/gamecodeschool/c4tappydefender/EnemyShip.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
-
 import java.util.Random;
 
 public class EnemyShip{
